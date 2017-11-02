6bee03bfe372870cd44aaebbf36bfe2f0906bc31
scottmo <scottscmo@gmail.com>
2017-11-01 18:22:46 -0700

    Change build process Make punctuations less catchy

--- tasks/build.js
+++ tasks/build.js
@@ -4,26 +4,40 @@ const engine = require('Mustache');
 
 const SRC = './src';
 const TARGET = './themes';
-const THEME_STYLES_DIR =  path.join(SRC, 'styles');
-const TEMPLATE_PATH = path.join(SRC, 'theme.json');
-const CHARSET = 'utf-8';
 
-const themeTemplate = fs.readFileSync(TEMPLATE_PATH, CHARSET);
-const themes = fs.readdirSync(THEME_STYLES_DIR)
-    .reduce(function(files, filename) {
-        const filePath = path.join(THEME_STYLES_DIR, filename);
-        const themeFile = fs.readFileSync(filePath, CHARSET);
+buildThemes('Momo', SRC, TARGET);
+
+// ------------------------------
+
+function withJsonExt(filename) {
+    return filename + '.json';
+}
+
+function loadJsonFiles(themesDir) {
+    return fs.readdirSync(themesDir).reduce(function(files, filename) {
+        const filePath = path.join(themesDir, filename);
+        const themeFile = fs.readFileSync(filePath, 'utf8');
         try {
             files.push(JSON.parse(themeFile));
         } catch (error) {
-            console.error('Failed to parse theme config', error);
+            console.error("Failed to parse theme config", error);
         }
         return files;
     }, []);
+}
 
-themes.forEach(config => {
-    const outputPath = path.join(TARGET, config.name + '.json');
-    const outputTheme = engine.render(themeTemplate, config);
-    fs.writeFileSync(outputPath, outputTheme, { encoding: CHARSET });
-    console.log('Output: ', outputPath);
-});
+function buildThemes(themplateName, srcDir, targetDir) {
+    const templatePath = path.join(srcDir, withJsonExt(themplateName));
+    const themesDir = path.join(srcDir, 'styles');
+    const themeTemplate = fs.readFileSync(templatePath, 'utf8');
+
+    loadJsonFiles(themesDir).forEach(config => {
+        config.name = themplateName + config.name;
+        const outputPath = path.join(targetDir, withJsonExt(config.name));
+        const outputTheme = engine.render(themeTemplate, config);
+        fs.writeFile(outputPath, outputTheme, 'utf8', err => {
+            if (err) throw err;
+            console.log('Output: ', outputPath);
+        }); 
+    });
+}
