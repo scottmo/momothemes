989fc5811de3f32d07e59f8970c0edb1ac45f96c
scottmo <scottscmo@gmail.com>
2021-07-09 11:59:28 -0500

    remove hard-coded style files

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,23 @@ const { src, dest, parallel } = require('gulp');
 const mustache = require('gulp-mustache');
 const rename = require('gulp-rename');
 const path = require('path');
-
-const SRC = 'src';
-const DST = 'build';
+const fs = require('fs');
 
 function buildThemes(done) {
-    const templateFile = path.join(SRC, 'Momo.json');
-    const dataFiles = [
-        'Dark',
-        'Light',
-        'OneDark',
-        'SolDark',
-        'SolLight'
-    ].map(name => name + '.json');
-
+    const templateFile = path.join('src', 'Momo.json');
+    const dataFiles = fs.readdirSync(path.join('src', 'styles'));
     dataFiles.forEach(dataFile => {
         src(templateFile)
-            .pipe(mustache(path.join(SRC, 'styles', dataFile)))
+            .pipe(mustache(path.join('src', 'styles', dataFile)))
             .pipe(rename('Momo' + dataFile))
-            .pipe(dest(path.join(DST, 'themes')));
+            .pipe(dest(path.join('build', 'themes')));
     });
     done();
 };
 
 function buildPackage(done) {
-    src('*.md').pipe(dest(DST));
-    src('package.json').pipe(dest(DST));
+    src('*.md').pipe(dest('build'));
+    src('package.json').pipe(dest('build'));
     done();
 };
 
