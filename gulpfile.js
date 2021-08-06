1376d0acf4005cd02b61c17e24716eeb36d175ff
scottmo <scottscmo@gmail.com>
2021-08-06 11:46:38 -0500

    update build process to modify package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,42 @@ const rename = require('gulp-rename');
 const path = require('path');
 const fs = require('fs');
 
+const themePrefix = 'Momo';
+const dataFiles = fs.readdirSync(path.join('src', 'styles'));
+
 function buildThemes(done) {
-    const templateFile = path.join('src', 'Momo.json');
-    const dataFiles = fs.readdirSync(path.join('src', 'styles'));
-    dataFiles.forEach(dataFile => {
+    const templateFile = path.join('src', 'Momo.json');    
+    dataFiles.forEach(fileName => {
         src(templateFile)
-            .pipe(mustache(path.join('src', 'styles', dataFile)))
-            .pipe(rename('Momo' + dataFile))
+            .pipe(mustache(path.join('src', 'styles', fileName)))
+            .pipe(rename(themePrefix + fileName))
             .pipe(dest(path.join('build', 'themes')));
     });
     done();
 };
 
 function buildPackage(done) {
+    buildPackageJSON();
+
     src('*.md').pipe(dest('build'));
-    src('package.json').pipe(dest('build'));
     done();
 };
+
+// update package.json with theme information and move it to build/
+function buildPackageJSON() {
+    const packageJson = JSON.parse(fs.readFileSync('./package.json'));
+    packageJson.contributes = { themes: [] };
+    dataFiles.forEach(fileName => {
+        const themeName = themePrefix +
+                fileName.substring(0, fileName.indexOf('.json'));
+        packageJson.contributes.themes.push({
+            label: themeName,
+            uiTheme: themeName.includes('Light') ? 'vs' : 'vs-dark',
+            path: `./themes/${themeName}.json`
+        });
+    });
+    fs.writeFileSync('./build/package.json', JSON.stringify(packageJson, null, 4));
+}
 
 exports.buildThemes = buildThemes;
 exports.buildPackage = buildPackage;
